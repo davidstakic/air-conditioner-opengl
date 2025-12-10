7675caa7614cd531b06d3565ba6841ca7c960233
David Stakic <davidstakic2003@gmail.com>
2025-12-11 00:48:57 +0100

    [REFACTOR] small changes

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -12,16 +12,16 @@
 #include <iostream>
 #include <string>
 
-bool klima_on = false;
-bool lavor_full = false;
-float precaga_offset = 0.0f;
-float precaga_speed = 0.0008f;
-float zeljenaTemp = 24.0f;
-float izmerenaTemp = 30.0f;
-float waterHeight = 0.0f;
+bool is_turned_on = false;
+bool is_full = false;
+float flap_offset = 0.0f;
+float flap_speed = 0.0008f;
+float desired_temperature = 24.0f;
+float measured_temperature = 31.0f;
+float water_height = 0.0f;
 
 GLFWcursor* cursor;
-GLFWcursor* cursorPressed;
+GLFWcursor* cursor_pressed;
 
 unsigned snowflake;
 unsigned fire;
@@ -38,7 +38,7 @@ bool isMouseOverCircle(double mouse_x, double mouse_y, Circle& c, int width, int
 
 void mouse_button_callback(GLFWwindow* window, int button, int action, int mods) {
     if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS) {
-        glfwSetCursor(window, cursorPressed);
+        glfwSetCursor(window, cursor_pressed);
 
         double mouseX, mouseY;
         glfwGetCursorPos(window, &mouseX, &mouseY);
@@ -46,12 +46,12 @@ void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
         int width, height;
         glfwGetFramebufferSize(window, &width, &height);
 
-        Circle* lampica = (Circle*)glfwGetWindowUserPointer(window);
-        if (!lampica) return;
+        Circle* light = (Circle*)glfwGetWindowUserPointer(window);
+        if (!light) return;
 
-        if (isMouseOverCircle(mouseX, mouseY, *lampica, width, height)) {
-            if (!klima_on && !lavor_full) klima_on = true;
-            else if (klima_on) klima_on = false;
+        if (isMouseOverCircle(mouseX, mouseY, *light, width, height)) {
+            if (!is_turned_on && !is_full) is_turned_on = true;
+            else if (is_turned_on) is_turned_on = false;
         }
     }
 
@@ -63,12 +63,12 @@ void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
 void keyboard_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
     if (action == GLFW_PRESS) {
         if (key == GLFW_KEY_UP)
-            zeljenaTemp = std::min(40.0f, zeljenaTemp + 1.0f);
+            desired_temperature = std::min(40.0f, desired_temperature + 1.0f);
         if (key == GLFW_KEY_DOWN)
-            zeljenaTemp = std::max(-10.0f, zeljenaTemp - 1.0f);
+            desired_temperature = std::max(-10.0f, desired_temperature - 1.0f);
         if (key == GLFW_KEY_SPACE) {
-            lavor_full = false;
-            waterHeight = 0.0f;
+            is_full = false;
+            water_height = 0.0f;
         }
         if (key == GLFW_KEY_ESCAPE)
             glfwSetWindowShouldClose(window, GLFW_TRUE);
@@ -110,8 +110,8 @@ int main() {
     unsigned int textShader = createShader("text.vert", "text.frag");
     unsigned int textureShader = createShader("texture.vert", "texture.frag");
 
-    TextRenderer textRenderer;
-    if (!textRenderer.init("Resources/DS-DIGI.TTF", textShader)) {
+    TextRenderer text_renderer;
+    if (!text_renderer.init("Resources/DS-DIGI.TTF", textShader)) {
         std::cerr << "Failed to initialize TextRenderer\n";
         return -1;
     }
@@ -120,18 +120,18 @@ int main() {
     glfwGetFramebufferSize(window, &fbw, &fbh);
     float aspect = (float)fbw / (float)fbh;
 
-    Rectangle klima(-0.378f, 0.6885f, 0.378f, 0.9415f);
-    Rectangle precaga_light(-0.304f, 0.692f, 0.304f, 0.728f);
-    Rectangle precaga_dark(-0.304f, 0.692f, 0.304f, 0.728f);
-    Circle lampica(0.355f, 0.71f, 0.010f, aspect);
+    Rectangle air_conditioner(-0.378f, 0.6885f, 0.378f, 0.9415f);
+    Rectangle flap_light(-0.304f, 0.692f, 0.304f, 0.728f);
+    Rectangle flap_dark(-0.304f, 0.692f, 0.304f, 0.728f);
+    Circle light(0.355f, 0.71f, 0.010f, aspect);
 
-    float ekranGap = 0.015f;
-    Rectangle ekran1(-0.20f - ekranGap, 0.8f, -0.12f - ekranGap, 0.88f);
-    Rectangle ekran2(-0.04f, 0.8f, 0.04f, 0.88f);
-    Rectangle ekran3(0.12f + ekranGap, 0.8f, 0.20f + ekranGap, 0.88f);
-    Rectangle lavor(-0.22f, -0.805f, 0.22f, -0.545f);
-    TexturedQuad iconQuad(0.14f + ekranGap, 0.81f, 0.18f + ekranGap, 0.87f);
-    TexturedQuad signatureQuad(-0.99f, -0.99f, -0.65f, -0.87f);
+    float screen_gap = 0.015f;
+    Rectangle screen_1(-0.20f - screen_gap, 0.8f, -0.12f - screen_gap, 0.88f);
+    Rectangle screen_2(-0.04f, 0.8f, 0.04f, 0.88f);
+    Rectangle screen_3(0.12f + screen_gap, 0.8f, 0.20f + screen_gap, 0.88f);
+    Rectangle basin(-0.22f, -0.805f, 0.22f, -0.545f);
+    TexturedQuad icon(0.14f + screen_gap, 0.81f, 0.18f + screen_gap, 0.87f);
+    TexturedQuad signature_texture(-0.99f, -0.99f, -0.65f, -0.87f);
 
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
@@ -142,33 +142,33 @@ int main() {
     preprocessTexture(check, "Resources/check.png");
     preprocessTexture(signature, "Resources/signature.png");
 
-    glfwSetWindowUserPointer(window, &lampica);
+    glfwSetWindowUserPointer(window, &light);
     glfwSetMouseButtonCallback(window, mouse_button_callback);
     glfwSetKeyCallback(window, keyboard_callback);
 
     cursor = loadImageToCursor("Resources/remote_control.png");
-    cursorPressed = loadImageToCursor("Resources/remote_control_pressed.png");
+    cursor_pressed = loadImageToCursor("Resources/remote_control_pressed.png");
     glfwSetCursor(window, cursor);
 
-    const double targetFPS = 75.0;
-    const double targetFrameTime = 1.0 / targetFPS;
+    const double target_fps = 75.0;
+    const double target_frame_time = 1.0 / target_fps;
 
-    const float waterTopCoord = -0.56f;
-    const float waterBottomCoord = -0.78f;
-    const float waterHeightMax = waterTopCoord - waterBottomCoord;
+    const float water_top_coord = -0.56f;
+    const float water_bottom_coord = -0.78f;
+    const float water_height_max = water_top_coord - water_bottom_coord;
     const float ABS_WATER_FILL_SPEED = 0.5f;
 
     while (!glfwWindowShouldClose(window)) {
-        double frameStart = glfwGetTime();
+        double frame_start = glfwGetTime();
 
         float max_offset = 0.036f;
-        if (klima_on) {
-            if (precaga_offset < max_offset) precaga_offset += precaga_speed;
-            if (izmerenaTemp < zeljenaTemp) izmerenaTemp = std::min(zeljenaTemp, izmerenaTemp + 0.01f);
-            else if (izmerenaTemp > zeljenaTemp) izmerenaTemp = std::max(zeljenaTemp, izmerenaTemp - 0.01f);
+        if (is_turned_on) {
+            if (flap_offset < max_offset) flap_offset += flap_speed;
+            if (measured_temperature < desired_temperature) measured_temperature = std::min(desired_temperature, measured_temperature + 0.01f);
+            else if (measured_temperature > desired_temperature) measured_temperature = std::max(desired_temperature, measured_temperature - 0.01f);
         }
         else {
-            if (precaga_offset > 0.0f) precaga_offset -= precaga_speed;
+            if (flap_offset > 0.0f) flap_offset -= flap_speed;
         }
 
         glClear(GL_COLOR_BUFFER_BIT);
@@ -180,38 +180,38 @@ int main() {
         int opacityLoc = glGetUniformLocation(textureShader, "uOpacity");
 
         if (uYOffsetLoc != -1) glUniform1f(uYOffsetLoc, 0.0f);
-        glUniform4f(colorLoc, 1.0f, 1.0f, 1.0f, 1.0f); klima.draw();
-        glUniform4f(colorLoc, 0.2f, 0.2f, 0.2f, 1.0f); precaga_dark.draw();
+        glUniform4f(colorLoc, 1.0f, 1.0f, 1.0f, 1.0f); air_conditioner.draw();
+        glUniform4f(colorLoc, 0.2f, 0.2f, 0.2f, 1.0f); flap_dark.draw();
 
-        if (uYOffsetLoc != -1) glUniform1f(uYOffsetLoc, precaga_offset);
-        glUniform4f(colorLoc, 0.5f, 0.5f, 0.5f, 1.0f); precaga_light.draw();
+        if (uYOffsetLoc != -1) glUniform1f(uYOffsetLoc, flap_offset);
+        glUniform4f(colorLoc, 0.5f, 0.5f, 0.5f, 1.0f); flap_light.draw();
 
         if (uYOffsetLoc != -1) glUniform1f(uYOffsetLoc, 0.0f);
-        if (klima_on) glUniform4f(colorLoc, 1.0f, 0.0f, 0.0f, 1.0f);
+        if (is_turned_on) glUniform4f(colorLoc, 1.0f, 0.0f, 0.0f, 1.0f);
         else glUniform4f(colorLoc, 0.2f, 0.2f, 0.2f, 1.0f);
-        lampica.draw();
+        light.draw();
 
         glUniform4f(colorLoc, 0.0f, 0.0f, 0.0f, 1.0f);
-        ekran1.draw(); ekran2.draw(); ekran3.draw();
+        screen_1.draw(); screen_2.draw(); screen_3.draw();
 
         glUniform4f(colorLoc, 0.8f, 0.8f, 0.8f, 0.3f);
-        lavor.draw();
+        basin.draw();
 
         glUniform4f(colorLoc, 0.1f, 0.7f, 0.8f, 1.0f);
-        float currentWaterTop = waterBottomCoord + waterHeight;
-        Rectangle dynamicWater(-0.205f, currentWaterTop, 0.205f, waterBottomCoord);
+        float currentWaterTop = water_bottom_coord + water_height;
+        Rectangle dynamicWater(-0.205f, currentWaterTop, 0.205f, water_bottom_coord);
         dynamicWater.draw();
 
-        if (klima_on) {
-            std::string s1 = std::to_string((int)zeljenaTemp);
-            std::string s2 = std::to_string((int)izmerenaTemp);
+        if (is_turned_on) {
+            std::string s1 = std::to_string((int)desired_temperature);
+            std::string s2 = std::to_string((int)measured_temperature);
 
-            textRenderer.render(s1, -0.203f, 0.825f, 0.0012f, Color(1, 1, 1));
-            textRenderer.render(s2, -0.028f, 0.825f, 0.0012f, Color(1, 1, 1));
+            text_renderer.render(s1, -0.203f, 0.825f, 0.0012f, Color(1, 1, 1));
+            text_renderer.render(s2, -0.028f, 0.825f, 0.0012f, Color(1, 1, 1));
 
-            if (izmerenaTemp < zeljenaTemp)
+            if (measured_temperature < desired_temperature)
                 glBindTexture(GL_TEXTURE_2D, fire);
-            else if (izmerenaTemp > zeljenaTemp)
+            else if (measured_temperature > desired_temperature)
                 glBindTexture(GL_TEXTURE_2D, snowflake);
             else
                 glBindTexture(GL_TEXTURE_2D, check);
@@ -219,44 +219,44 @@ int main() {
             glUseProgram(textureShader);
             glActiveTexture(GL_TEXTURE0);
             glBindTexture(GL_TEXTURE_2D,
-                izmerenaTemp < zeljenaTemp ? fire :
-                izmerenaTemp > zeljenaTemp ? snowflake : check);
+                measured_temperature < desired_temperature ? fire :
+                measured_temperature > desired_temperature ? snowflake : check);
 
             glUniform1i(texLoc, 0);
             glUniform1f(opacityLoc, 1.0f);
-            iconQuad.draw();
+            icon.draw();
         }
 
         glUseProgram(textureShader);
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, signature);
         glUniform1f(opacityLoc, 0.5f);
-        signatureQuad.draw();
+        signature_texture.draw();
 
         glfwSwapBuffers(window);
         glfwPollEvents();
 
-        double frameEnd = glfwGetTime();
-        double frameTime = frameEnd - frameStart;
+        double frame_end = glfwGetTime();
+        double frame_time = frame_end - frame_start;
 
-        if (klima_on && !lavor_full) {
-            waterHeight += ABS_WATER_FILL_SPEED * frameTime;
-            if (waterHeight >= waterHeightMax) {
-                waterHeight = waterHeightMax;
-                lavor_full = true;
-                klima_on = false;
+        if (is_turned_on && !is_full) {
+            water_height += ABS_WATER_FILL_SPEED * frame_time;
+            if (water_height >= water_height_max) {
+                water_height = water_height_max;
+                is_full = true;
+                is_turned_on = false;
             }
         }
 
-        double sleepTime = targetFrameTime - frameTime;
-        if (sleepTime > 0.0) {
-            auto sleepDur = std::chrono::duration<double>(sleepTime);
-            std::this_thread::sleep_for(std::chrono::duration_cast<std::chrono::milliseconds>(sleepDur));
-            while (glfwGetTime() - frameStart < targetFrameTime) {}
+        double sleep_time = target_frame_time - frame_time;
+        if (sleep_time > 0.0) {
+            auto sleep_dur = std::chrono::duration<double>(sleep_time);
+            std::this_thread::sleep_for(std::chrono::duration_cast<std::chrono::milliseconds>(sleep_dur));
+            while (glfwGetTime() - frame_start < target_frame_time) {}
         }
     }
 
-    textRenderer.cleanup();
+    text_renderer.cleanup();
     glfwDestroyWindow(window);
     glfwTerminate();
     return 0;
